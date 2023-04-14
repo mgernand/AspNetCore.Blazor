d636b8a1194b8abbc06d0220915c639c12fb6ac1
Matthias Gernand <mgernand@users.noreply.github.com>
2023-04-15 01:33:27 +0200

    Added extension to get a query string parameter value. (#10)

---
+++ src/AspNetCore.Blazor/NavigationManagerExtensions.cs
@@ -0,0 +1,55 @@
+﻿namespace MadEyeMatt.AspNetCore.Blazor
+{
+	using System;
+	using JetBrains.Annotations;
+	using Microsoft.AspNetCore.Components;
+	using Microsoft.AspNetCore.WebUtilities;
+	using Microsoft.Extensions.Primitives;
+
+	/// <summary>
+	///     Extension methods for the <see cref="NavigationManager" /> type.
+	/// </summary>
+	/// <remarks>
+	///     Taken from: https://chrissainty.com/working-with-query-strings-in-blazor/
+	/// </remarks>
+	[PublicAPI]
+	public static class NavigationManagerExtensions
+	{
+		/// <summary>
+		///     Tries to get the typed value of a query string parameter.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="navigationManager"></param>
+		/// <param name="parameterName"></param>
+		/// <param name="value"></param>
+		/// <returns></returns>
+		public static bool TryGetQueryParameter<T>(this NavigationManager navigationManager, string parameterName, out T value)
+		{
+			Uri uri = navigationManager.ToAbsoluteUri(navigationManager.Uri);
+
+			if(QueryHelpers.ParseQuery(uri.Query).TryGetValue(parameterName, out StringValues valueFromQueryString))
+			{
+				if(typeof(T) == typeof(int) && int.TryParse(valueFromQueryString, out int valueAsInt))
+				{
+					value = (T)(object)valueAsInt;
+					return true;
+				}
+
+				if(typeof(T) == typeof(string))
+				{
+					value = (T)(object)valueFromQueryString.ToString();
+					return true;
+				}
+
+				if(typeof(T) == typeof(decimal) && decimal.TryParse(valueFromQueryString, out decimal valueAsDecimal))
+				{
+					value = (T)(object)valueAsDecimal;
+					return true;
+				}
+			}
+
+			value = default;
+			return false;
+		}
+	}
+}
